34b76b2229b794b2201d77f1b5bac1ee117c0c2a
milalina <ludmilla.mamelina@gmail.com>
2019-11-27 17:48:43 +0100

    moving element complete & buttons

--- main.js
+++ main.js
@@ -11,22 +11,21 @@ function generate_tetris_grid(){
         for (var j=0; j<cols; j++) {
           var cell = document.createElement("td");
           cell.id="r"+i+"c"+j
-          //var cellText = document.createTextNode("cell in row "+i+", column "+j);
-          //cell.appendChild(cellText);
           row.appendChild(cell);
         }
         tblBody.appendChild(row);
     }
  grid.appendChild(tblBody);
  grid.setAttribute("border", "2");
- //element_moving_vertically()
- callShortT()
+ createArrayForDisplayingShortTverticalMovement()
 } 
 
- function callShortT(){
+ function createArrayForDisplayingShortTverticalMovement(){
   var k;
+  var l;
   var i;
-  var short_T_as_param=[];
+  var show_short_T_as_param=[];
+  var remove_short_T_as_param=[];
   for(var k=0; k<17; k++){
     i=k;
     j=5;
@@ -36,23 +35,29 @@ function generate_tetris_grid(){
     var c = "r"+(i)+"c"+(++j);
     var d = "r"+(i)+"c"+(++j);
     short_T.push(a,b,c,d);
-    short_T_as_param.push(short_T)
-    //console.log(short_T_as_param)
-   // short_T=[]
+    show_short_T_as_param.push(short_T)
   }
-  display_element_moving_vertically(short_T_as_param);
- // remove_element_moving_vertically(short_T_as_param);
-  console.log(short_T_as_param)
+  for(var l=0; l<16; l++){
+    i=l;
+    j=5;
+    var remove_short_T=[];
+    var a = "r"+i+"c"+j;
+    var b = "r"+(++i)+"c"+(--j);
+    var c = "r"+(i)+"c"+(++j);
+    var d = "r"+(i)+"c"+(++j);
+    remove_short_T.push(a,b,d);
+    remove_short_T_as_param.push(remove_short_T)
+  }
+  display_element_moving_vertically_position_1(show_short_T_as_param, remove_short_T_as_param);
 } 
 
-function display_element_moving_vertically(array){
+function display_element_moving_vertically_position_1(array1, array2){
    animate_show(0)
       function animate_show(j){
-        a=array[j];
-        console.log(a)
+        a=array1[j];
         myVar=setTimeout(function(){
           for (l in a){document.getElementById(a[l]+"").style.backgroundColor="salmon"}
-        if (j==16){
+        if (j==17){
           clearTimeout(myVar);
           return;
         }animate_show(j+1)}
@@ -63,8 +68,7 @@ function display_element_moving_vertically(array){
         myVar=setTimeout(function(){
          if(j>0 && j<19){
            i=j-1;
-            b=array[i];
-            console.log(b)
+            b=array2[i];
           for(l in b){document.getElementById(b[l]+"").style.backgroundColor="white"}}
         if (j==19){
           clearTimeout(myVar);
@@ -73,30 +77,3 @@ function display_element_moving_vertically(array){
          , 500)
       }
 }
-
-/* function remove_element_moving_vertically(array){
-  animate(0)
-  function animate(j){
-    a=array[j];
-    console.log(a)
-    myVar=setTimeout(function(){
-      for (l in a){document.getElementById(a[l]+"").style.backgroundColor="white"}
-    if (j==16){
-      clearTimeout(myVar);
-      return;
-    }animate(j+1)}
-     , 1500)
-  }
-} */
-
-/*  function element_moving_vertically(array){
-  for(i in array){
-    a=array[i]
-    for(j in a){
-      setTimeout(function(){document.getElementById(a[j]+"").style.backgroundColor="salmon"}, 500, j);
-   
-    }
-  }
-} */
- 
-// setTimeout(()=> {k++}, 1000)  document.getElementById(a[j]+"").style.backgroundColor="salmon" 
