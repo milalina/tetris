53494cfd0c360e695c974c8c12298574ee7bf606
milalina <ludmilla.mamelina@gmail.com>
2019-12-13 12:33:31 +0100

    rotation of element

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var x_cols_left = [];
 var x_cols_right = [];
+var rotate_clockwise = [];
+var place_element_bottom =[];
 var col = 5;
 var objectForRemovingElements = [];
 
@@ -28,23 +30,82 @@ function generate_tetris_grid() {
 }
 
 function animate_show(j) {
-  //create short-T element per row j for displaying it    
+  //create short-T element per row j for displaying it  
+  var time;  
+  if(place_element_bottom.length==1){
+    time=100
+  }else(time=500)
   myVar = setTimeout(function () {
+    var final_row;
     var row = j;
     var move_x_rows = x_cols_right.length - x_cols_left.length;
     col = col + move_x_rows;
-    if(col<=1){
-      col=1
-    }if(col>=8){
-      col=8
+    //the element points up (square a faces up)
+    if (rotate_clockwise.length == 0 || rotate_clockwise.length == 4) {
+      if (col <= 1) {
+        col = 1
+      }
+      if (col >= 8) {
+        col = 8
+      }
+      var short_T = [];
+      var a = "r" + row + "c" + col;
+      var b = "r" + (++row) + "c" + (--col);
+      var c = "r" + (row) + "c" + (++col);
+      var d = "r" + (row) + "c" + (++col);
+      col = col - 1; //compensate for shift of columns as a result of additions in var a,b,c,d
+      short_T.push(a, b, c, d);
+      rotate_clockwise=[];
+      final_row=17;
     }
-    var short_T = [];
-    var a = "r" + row + "c" + col;
-    var b = "r" + (++row) + "c" + (--col);
-    var c = "r" + (row) + "c" + (++col);
-    var d = "r" + (row) + "c" + (++col);
-    col = col - 1; //compensate for shift of columns as a result of additions in var a,b,c,d
-    short_T.push(a, b, c, d);
+     //the element points right (square a faces right)
+     if (rotate_clockwise.length == 1) {
+      if (col <= 0) {
+        col = 0
+      }
+      if (col >= 8) {
+        col = 8
+      }
+      var short_T = [];
+      var b = "r" + row + "c" + col;
+      var c = "r" + (++row) + "c" + (col);
+      var a = "r" + (row) + "c" + (++col);
+      var d = "r" + (++row) + "c" + (--col);
+      short_T.push(a, b, c, d);
+      final_row=15;
+    }
+    //the element points down (square a faces down)
+    if (rotate_clockwise.length == 2) {
+      if (col <= 1) {
+        col = 1
+      }
+      if (col >= 8) {
+        col = 8
+      }
+      var short_T = [];
+      var c = "r" + (row) + "c" + (col);
+      var d = "r" + row + "c" + (--col);
+      var b = "r" + (row) + "c" + (col+2);
+      var a = "r" + (++row) + "c" + (++col);
+      short_T.push( a, b, c, d);
+      final_row=17;
+    }
+    //the element points left (square a faces left)
+    if (rotate_clockwise.length == 3) {
+      if (col <= 1) {
+        col = 1
+      }
+      if (col >= 9) {
+        col = 9
+      }
+      var short_T = [];
+      var c = "r" + (row) + "c" + (col);
+      var a = "r" + row + "c" + (--col);
+      var d = "r" + (--row) + "c" + (++col);
+      var b = "r" + (row+2) + "c" + (col);
+      short_T.push( a, b, c, d);
+      final_row=16;
+    }
     a = short_T;
     x_cols_right = [];
     x_cols_left = [];
@@ -52,16 +113,23 @@ function animate_show(j) {
     for (l in a) {
       document.getElementById(a[l] + "").className = "salmon"
     }
-    console.log("show row"+j)
-    if (j == 17) {
+    console.log("show row" + j)
+
+    if (j == final_row) {
       clearTimeout(myVar);
       return;
     }
     animate_show(j + 1)
-  }, 500)
+    place_element_bottom=[]
+  }, time)
 }
+
 function animate_hide(j) {
   //remove element
+  var time;
+  if(place_element_bottom.length==1){
+    time=99
+  }else(time=499)
   myVar = setTimeout(function () {
     if (j > 0 && j < 17) {
       var salmon_table_cells = document.querySelectorAll(".salmon");
@@ -69,13 +137,14 @@ function animate_hide(j) {
         cell.className = "white";
       });
     }
-    console.log("remove row"+j)
+    console.log("remove row" + j)
     if (j == 17) {
       clearTimeout(myVar);
       return;
     }
     animate_hide(j + 1)
-  }, 499)
+    place_element_bottom=[]
+  }, time)
 }
 // changing position of elements on the grid
 function add_event_listeners_to_buttons() {
@@ -85,13 +154,12 @@ function add_event_listeners_to_buttons() {
   document.getElementById("right").addEventListener("click", function () {
     move_x_cols_right(1);
   });
-  /*  document.getElementById("rotate").addEventListener("click", function () {
-     myFunction(p1, p2);
-   });
+  document.getElementById("rotate").addEventListener("click", function () {
+    rotate_clockwise_a_right_down_left_up(1);
+  }); 
    document.getElementById("down").addEventListener("click", function () {
-     myFunction(p1, p2);
-   }); */
-  // document.getElementById("display_movement_feft_right").innerHTML="move right " + x_cols_right[0] + " rows," +"move left " + x_cols_left[0] + " rows"
+     place_element_down(1);
+   }); 
 }
 
 function move_x_cols_left(x) {
@@ -100,4 +168,12 @@ function move_x_cols_left(x) {
 
 function move_x_cols_right(x) {
   x_cols_right.push(x)
+}
+
+function rotate_clockwise_a_right_down_left_up(x) {
+  rotate_clockwise.push(x)
+}
+
+function place_element_down(x){
+  place_element_bottom.push(x)
 }
